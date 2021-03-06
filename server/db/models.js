060ef0a40807bcffc989c35d648df6122ea78fbb
klooghbear <sebastiankloogh@gmail.com>
2021-03-06 22:50:34 +1300

    initial eslint config

--- server/db/models.js
+++ server/db/models.js
@@ -1,9 +1,9 @@
-const connection = require('./connection');
+const connection = require("./connection")
 
 const getter = (db = connection) => {
-  return db('some_table').select();
+  return db("some_table").select()
 }
 
 module.exports = {
   getter,
-};
+}
